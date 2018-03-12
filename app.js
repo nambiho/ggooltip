a566db22eacd4e932a42ccabafe58c1a6cda7426
NambihoMac <nambihomac@NambihoMacui-MacBook-Pro.local>
2018-03-12 16:48:38 +0900

    app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 8000);
+app.set('rootdir', __dirname);
 
 app.listen(app.get('port'), function (req, res) {
 	console.log('Express server is running on port ' + app.get('port'))
