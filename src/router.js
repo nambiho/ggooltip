79466eb9c2a51fe70e5c0271772146340b199d0d
NambihoMac <nambihomac@NambihoMacui-MacBook-Pro.local>
2018-03-12 16:49:13 +0900

    router.js

--- src/router.js
+++ src/router.js
@@ -1,7 +1,23 @@
 "use strict";
 
+import fs from "fs";
+
 function router (app) {
-	console.log(app)
+	const __rootdir = app.get('rootdir');
+
+	app.get('/', function (req, res) {
+		res.sendFile(__rootdir + '/index.html');
+	});
+	app.get('/assets/js/:jsfile', function (req, res) {
+		// const jsfile = req.params.jsfile;
+		// if (!jsfile) {res.end(); return}
+		fs.stat(__rootdir + req.url, (err, stat) => {
+			if (err) {console.log(err); return}
+			console.log(__rootdir + req.url);
+			res.sendFile(__rootdir + req.url);
+			res.end();
+		});
+	});
 }
 
 export default router;
