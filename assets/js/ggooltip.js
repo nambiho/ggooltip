1885aa17cd8ed7f8a8c2cd98a1e7d0bdf05b19d3
NambihoMac <nambihomac@NambihoMacui-MacBook-Pro.local>
2018-03-13 02:33:41 +0900

    ggooltip.js

--- assets/js/ggooltip.js
+++ assets/js/ggooltip.js
@@ -2,14 +2,18 @@
 	global['gg'] = typeof exports === 'object' || typeof modules === 'object' ? null : gg;
 } (this, (function () {
 	"use strict";
+	var sl = new simplelib();
 
-	var define = {
-		"design":{
-			top:0
-		}
-	};
+	var gg = function (option) {
+		var element = sl.util.createElement({
+			dom:"div",
+			text:option.message,
+			style:{
+				background:"#eee",
+				color:"black"
+			}
+		}, document);
+	}
 
-	var gg = Object.create(define);
-
-	return gg;
+	return gg
 } ())));
