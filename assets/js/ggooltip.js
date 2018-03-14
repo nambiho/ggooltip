f0240bccce61f27b7ae58b2bbbdf00492421a491
NambihoMac <nambihomac@NambihoMacui-MacBook-Pro.local>
2018-03-15 00:35:59 +0900

    ggooltip.js

--- assets/js/ggooltip.js
+++ assets/js/ggooltip.js
@@ -5,17 +5,34 @@
 	var sl = new simplelib();
 
 	var gg = function (option) {
-		var element = sl.util.createElement({
+		var frame = sl.util.createElement({
 			dom:"div",
-			text:option.message,
 			style:{
 				background:"#eee",
 				color:"black",
-				display:"inline-block",
+				display:"inline-flex",
 				padding:"10px"
 			}
 		});
-		document.body.appendChild(element);
+		var element = sl.util.createElement({
+			dom:"div",
+			text:option.message,
+			parent:frame,
+			style:{
+				float:"left",
+				paddingRight:"10px"
+			}
+		});
+		var x = sl.util.createElement({
+			dom:"div",
+			parent:frame,
+			html:'<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'+
+				'<line x1="0" y1="0" x2="100%" y2="100%" stroke="black" stroke-width="1" />'+
+				'<line x1="0" y1="100%" x2="100%" y2="0" stroke="black" stroke-width="1" />'+
+				'</svg>'
+		});
+
+		document.body.appendChild(frame);
 	}
 
 	return gg
