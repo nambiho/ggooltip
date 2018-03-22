cb2d40f434b9e1d55bc9c09c001c4fe27eb0003e
nambiho.moon <nambiho.moon@gmail.com>
2018-03-22 10:01:39 +0900

    app.js

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const express = require('express');
 
 const app = express();
 
+const path = require('path');
+
 app.set('port', process.env.PORT || 8000);
 app.set('rootdir', __dirname);
+
+
+app.use('/js', express.static(path.join(__dirname, 'assets/js')));
+app.use('/css', express.static(path.join(__dirname, 'assets/css')));
 
 app.listen(app.get('port'), function (req, res) {
 	console.log('Express server is running on port ' + app.get('port'))
