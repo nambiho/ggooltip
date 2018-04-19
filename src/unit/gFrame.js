261369a8b377878c22a72aaaf699bb33a0372bd8
nambiho.moon <nambiho.moon@gmail.com>
2018-04-20 04:59:54 +0900

    gFrame.js

--- src/unit/gFrame.js
+++ src/unit/gFrame.js
@@ -1,46 +1,88 @@
 module.exports = function (sl) {
 	'use strict';
 
-	var gapX, gapY, posX, posY, _move;
+	var moveEvent,
+	elemPosition = {
+		gap : {x:0, y:0},
+		pos : {x:0, y:0},
+		real : {x:0, y:0},
+		left:0,
+		top:0,
+		init (elem, evt) {
+			var rect = elem.getBoundingClientRect();
+			// real
+			this.real.x = elem.offsetLeft - rect.left;
+			this.real.y = elem.offsetTop - rect.top;
+			// gap
+			this.gap.x = evt.pageX - rect.x - this.real.x;
+			this.gap.y = evt.pageY - rect.y - this.real.y;
+			// pos
+			this.pos.x = evt.pageX - rect.x;
+			this.pos.y = evt.pageY - rect.y;
+			// left
+			this.left = rect.width - this.pos.x;
+			this.top = rect.height - this.pos.y;
+			// size
+			this.width = Math.ceil(rect.width - this.real.x);
+			this.height = Math.ceil(rect.height - this.real.y);
+		},
+		getXY (elem, evt) {
+			var x, y, rect = elem.getBoundingClientRect();
+			if (evt.pageX - elemPosition.pos.x < 0) x = elemPosition.real.x
+			else x = evt.pageX - elemPosition.gap.x
 
-	function renderMove (elem) {
-		return (e) => {
-			var x = e.pageX - gapX,
-			y = e.pageY - gapY,
-			rect = elem.getBoundingClientRect()
-			// limitLeft = (e.clientX - posX),
-			// limitTop = (e.clientY - posY);
-			// console.log(e.clientX, posX)
+			if (evt.pageY - elemPosition.pos.y < 0) y = elemPosition.real.y
+			else y = evt.pageY - elemPosition.gap.y
+
+			var winWidth = window.innerWidth,
+			winHeight = window.innerHeight,
+			right = winWidth - (window.event.pageX + elemPosition.left),
+			bottom = winHeight - (window.event.pageY + elemPosition.top)
 			;
-			if (e.clientX < 0) x = posX;
-			if (e.clientY < 0) y = posY;
 
-			elem.style.left = x + 'px';
-			elem.style.top = y + 'px';
+			if (right <= 0) x = (winWidth - this.width) - 1;
+			if (bottom <= 0) y = (winHeight - this.height) - 1;
+
+			return {x:x,y:y}
 		}
 	}
 
+	function getMoveEvent (elem) {
+		return (e) => {
+			var xy = elemPosition.getXY(elem, e);
+			elem.style.left = xy.x + 'px';
+			elem.style.top = xy.y + 'px';
+		}
+	}
+
+	function getUpEvent (elem) {
+		return (e) => {
+			sl.util.trigger(elem, 'mouseup');
+		}
+	}
+	var upEvent;
 	function mouseDown(e) {
-		var rect = this.getBoundingClientRect();
-		// e.pageX : click point
-		// rect.x : computed left position
-		// this.offsetLeft : real left position
-		// 
-
-		gapX = e.pageX - rect.x - (this.offsetLeft-rect.x);
-		gapY = e.pageY - rect.y - (this.offsetTop-rect.y);
-		posX = e.clientX - rect.x;
-		posY = e.clientY - rect.y;
-
 		if(e.button !== 0) return;
-		_move = renderMove(this);
+
+		var rect = this.getBoundingClientRect();
+		elemPosition.init(this, e);
+
+		moveEvent = getMoveEvent(this);
+		upEvent = getUpEvent(this);
+
 		this.classList.add('unselectable');
-		sl.util.addEvent(document, 'mousemove', _move)
+		sl.util.addEvent(window, {
+			'mousemove': moveEvent,
+			'mouseup': upEvent
+		})
 	}
 
 	function mouseUp (e) {
 		this.classList.remove('unselectable');
-		sl.util.removeEvent(document, 'mousemove', _move)
+		sl.util.removeEvent(window, {
+			'mousemove': moveEvent,
+			'mouseup': upEvent
+		})
 	}
 
 	return function (config) {
