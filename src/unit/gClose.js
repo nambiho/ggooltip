41f97f218db7e23b7f6cd00ea7956dbe985afffd
nambiho.moon <nambiho.moon@gmail.com>
2018-04-23 15:18:24 +0900

    ggooltip message

--- src/unit/gClose.js
+++ src/unit/gClose.js
@@ -14,18 +14,18 @@ module.exports = function (sl) {
 			dom: 'div',
 			parent: config.frame,
 			html: '<svg width="20px" height="20px" version="1.1" xmlns="http://www.w3.org/2000/svg">'+
-				'<circle cx="50%" cy="50%" r="49%" stroke="black" stroke-width="0.5" fill="white" />'+
+				'<circle cx="50%" cy="50%" r="48%" stroke="black" stroke-width="1" fill="white" />'+
 				'<line x1="25%" y1="25%" x2="75%" y2="75%" stroke="black" stroke-width="1" />'+
 				'<line x1="25%" y1="75%" x2="75%" y2="25%" stroke="black" stroke-width="1" />'+
 				'</svg>',
 			style: {
-				cursor:'pointer',
-				position:'relative',
+				cursor: 'pointer',
+				position: 'relative',
 				// border:'1px solid gray',
-				right:'0',
+				right: '0',
 				margin: '12px 0',
-				padding:0,
-				display:'inline-flex'
+				padding: 0,
+				display: 'inline-flex'
 			},
 			event: {
 				click: removeElement.call(config.frame)
